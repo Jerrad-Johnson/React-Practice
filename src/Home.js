2d1c22f1789aee1425efca3471ba15e0d22ae9f0
Jerrad-Johnson <j_johnson21@mail.fhsu.edu>
2022-03-16 23:58:17 -0600

    Added error handling to fetch.

--- src/Home.js
+++ src/Home.js
@@ -9,21 +9,29 @@ const Home = () => {
 
 const [colors, setColors] = useState(null);
 const [isPending, setIsPending] = useState(true);
+const [error, setError] = useState(null);
 
 useEffect(() => {
     fetch('http://localhost:8000/colors')
         .then(response => {
+            if (!response.ok) {
+                throw Error("No response from the server.");
+            }
             return response.json();
         })
         .then(data => {
             setColors(data);
+            setIsPending(false)
+        }).catch(err => {
+            setError(err.message);
             setIsPending(false);
-        });
+    });
     }, []);
 
 
     return (
         <div className="Home">
+            {error && <div> {error} </div>}
             {isPending && <div>Loading...</div>}
             {colors && <Array colors={colors} />}
         </div>
