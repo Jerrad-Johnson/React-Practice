ee664a4d555a0ea48be2e0bf99b6948d8e887244
Jerrad-Johnson <j_johnson21@mail.fhsu.edu>
2022-03-18 11:21:27 -0600

    Added form submission (not yet linked to databse) to Create.js.

--- src/Create.js
+++ src/Create.js
@@ -3,12 +3,18 @@ import {useState} from "react";
 const Create = () => {
     const [color, setColor] = useState('');
     const [value, setValue] = useState('');
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const entry = {color, value};
+
+        console.log(entry);
+    }
 
     return (
         <div className="create">
             <h2>Add a blog</h2>
             <br />
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Color:</label>
                 <textarea
                     required
@@ -22,7 +28,8 @@ const Create = () => {
                     value={value}
                     onChange={(v) => setValue(v.target.value)}
                 ></textarea>
-
+                <br /><br />
+                <input type="submit"></input>
             </form>
         </div>
     );
