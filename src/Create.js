25855f9a8d376bde68fe0321b4a0ad8705a7746f
Jerrad-Johnson <j_johnson21@mail.fhsu.edu>
2022-03-18 00:39:27 -0600

    Prepared create page for form submission and array creation.

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,29 @@
+import {useState} from "react";
+
 const Create = () => {
+    const [color, setColor] = useState('');
+    const [value, setValue] = useState('');
+
     return (
         <div className="create">
             <h2>Add a blog</h2>
+            <br />
+            <form>
+                <label>Color:</label>
+                <textarea
+                    required
+                    value={color}
+                    onChange={(c) => setColor(c.target.value)}
+                ></textarea>
+                <br /><br />
+                <label>Value:</label>
+                <textarea
+                    required
+                    value={value}
+                    onChange={(v) => setValue(v.target.value)}
+                ></textarea>
+
+            </form>
         </div>
     );
 }
