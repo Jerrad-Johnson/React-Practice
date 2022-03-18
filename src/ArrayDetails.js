1ddf9a1a1d9868126047f9b534d4d8b121365628
Jerrad-Johnson <j_johnson21@mail.fhsu.edu>
2022-03-17 22:59:06 -0600

    Reused the useFetch custom hook in ArrayDetails to fetch a single json entry.

--- src/ArrayDetails.js
+++ src/ArrayDetails.js
@@ -1,11 +1,20 @@
 import {useParams} from "react-router-dom";
+import useFetch from "./useFetch";
 
 const ArrayDetails = () => {
     const { id } = useParams();
-
+    let url = "http://localhost:8000/colors/" + id;
+    const { data: singleColor, error, isPending } = useFetch(url);
     return (
         <div className="array-details">
-            <h2>Array Details -- { id }</h2>
+            { isPending && <div>Loading...</div>}
+            { error && <div>{ error}</div> }
+            { singleColor && (
+                <div>
+                    <div>Color: {singleColor.value}</div>
+                    <div>id: {singleColor.id}</div>
+                </div>
+                )}
         </div>
     );
 }
